1b8641d3c901e6444a94903a2a330101af60bd1e
Lucas Bastian <bastian.lucas@icloud.com>
2023-04-29 14:43:29 +0200

    Postegre to JSON

--- src/classes/postgre/PostgreTable.ts
+++ src/classes/postgre/PostgreTable.ts
@@ -1,11 +1,11 @@
 import * as path from "path";
 import * as fs from "fs";
 
-import { Path } from "src/utils/Path/Path";
+import { Path } from "../../utils/Path/Path";
 import { PostgreRecord } from "./PostgreRecord";
 
 abstract class PostgreTable<R extends PostgreRecord<Object>> {
-	private static readonly __PATH_TO_OUTPUT = path.resolve(Path.pathToJson, "tmp/tables");
+	private static readonly __PATH_TO_OUTPUT = path.resolve(Path.pathToJson, "tmp/postgre");
 	private static readonly __JSON_LIMIT: number = 1000;
 
 	private readonly __name: string;
